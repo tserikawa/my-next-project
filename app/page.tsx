1600128cb2e28c4c6075d2724d10d933842f8734
tserikawa <tkt.srkw@gmail.com>
2024-09-18 23:06:04 +0900

    3-2-1ファイルの扱い方

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグローバルテックカンパニーです。
-      </p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルテックカンパニーです。
+        </p>
+      </div>
+      <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
+    </section>
   );
 }
